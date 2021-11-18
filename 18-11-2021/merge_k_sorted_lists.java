b0378795e082226196f6d8612f87095cd2ef04c1
sahil sandhu <46192764+sahilsandhu@users.noreply.github.com>
2021-11-18 13:00:16 +0530

    Create merge_k_sorted_lists.java

---
+++ 18-11-2021/merge_k_sorted_lists.java
@@ -0,0 +1,83 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2)
+        {
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
+        ListNode prev = null;
+        ListNode dummy = null;
+        while(l1!=null && l2!=null)
+        {
+            if(l1.val < l2.val)
+            {
+                if(dummy == null)
+                {
+                    dummy = l1;
+                    prev = dummy;
+                }
+                else
+                {
+                    prev.next = l1;
+                    prev = prev.next;
+                }
+                l1 = l1.next;
+            }
+            else
+            {
+                if(dummy == null)
+                {
+                    dummy = l2;
+                    prev = dummy;
+                }
+                else
+                {
+                    prev.next = l2;
+                    prev = prev.next;
+                }
+                l2 = l2.next;
+            }
+        }
+        if(l1!=null)
+        {
+           prev.next =  l1;
+        }
+        else if(l2!=null)
+        {
+            prev.next = l2;
+        }
+            
+        return dummy;
+    }
+    
+    public  ListNode mergerSortLists(ListNode[] lists, int beg,int end)
+    {
+        if(beg == end)
+        {
+            return lists[beg];
+        }
+        int mid = (beg + end)/2;
+        ListNode l1 = mergerSortLists(lists,beg,mid);
+        ListNode l2 = mergerSortLists(lists,mid+1,end);
+        return mergeTwoLists(l1,l2);
+    }
+
+    
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0)
+            return null;
+        if(lists.length == 1)
+            return lists[0];
+        return mergerSortLists(lists,0,lists.length-1);
+    }
+}
