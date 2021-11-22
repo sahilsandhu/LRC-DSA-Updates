5f9fcb7943985ba1e9ea7de6039b193e1ba4e4f1
sahil sandhu <46192764+sahilsandhu@users.noreply.github.com>
2021-11-22 11:12:37 +0530

    Create inorder.java

---
+++ 21-11-2021/inorder.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class state{
+    TreeNode node;
+    boolean sd;
+    boolean ld;
+    boolean rd;
+    state(TreeNode node, boolean sd,boolean ld,boolean rd)
+    {
+        this.node = node;
+        this.sd = sd;
+        this.ld = ld;
+        this.rd = rd;
+    }
+}
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root == null)
+            return new ArrayList<>();
+        Stack<state> st = new Stack<>();
+        List<Integer> ans = new ArrayList<>();
+        st.push(new state(root,false,false,false));
+        while(st.size()>0)
+        {
+            state rp = st.peek();
+            // st.pop();
+            if(rp.ld == false)
+            {
+                if(rp.node.left != null)
+                st.push(new state(rp.node.left,false,false,false));
+                
+                rp.ld = true;
+            }
+            else if(rp.sd == false)
+            {
+                
+                ans.add(rp.node.val);
+                // st.push(new state(rp.node,true,rp.ld,rp.rd));
+                rp.sd = true;
+            }
+            else if(rp.rd == false)
+            {
+                if(rp.node.right!=null)
+                st.push(new state(rp.node.right,false,false,false));
+                rp.rd = true;
+            }  
+            else
+            {
+                st.pop();
+            }
+        }
+        return ans;
+    }
+}
